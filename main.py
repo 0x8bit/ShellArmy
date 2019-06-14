508b57be7428c2569c42448de692f71a0a0ab1df
Adebayo Ojo Ayowamide <24305638+0x8bit@users.noreply.github.com>
2019-06-14 16:37:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+print('\n\t\t#################################################################')
+print('\t\t#                                                               #')
+print('\t\t#                          Shell_Army                           #')
+print('\t\t#                                                               #')
+print('\t\t#             Author:     0x8bit                                #')
+print('\t\t#             Reference:  Red and blue team field manual        #')
+print('\t\t#             Donate btc: 17SaudgRmKSDEA5fNA4os2pPSsgknipLnm    #')
+print('\t\t#                                                               #')
+print('\t\t#################################################################\n')
+
+if __name__ == '__main__':
+    while True:
+        print('\n**Switch Platform**')
+        print("""
+        [1] Linux
+        [2] Windows
+        [3] Quit
+        """)
+        user_input = input('__Choose your weapon >__ ')
+        if user_input == '1':
+            while True:
+                    try:
+                        from ShellArmy.linux import lin_main
+                    except SystemExit:
+                        break
+        elif user_input == '2':
+            while True:
+                try:
+                    from ShellArmy.windows import win_main
+                except SystemExit:
+                    break
+        elif user_input in ['quit', 'Quit', '3', 'exit', 'Exit']:
+            break
+        else:
+            print("You're Drunk!")
