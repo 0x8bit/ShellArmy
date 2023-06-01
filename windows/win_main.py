26aabe78063ac3ec0df491c4510a47c6cb4741f8
0x8bit <24305638+0x8bit@users.noreply.github.com>
2023-06-01 20:22:26 +0100

    Update win_main.py

--- windows/win_main.py
+++ windows/win_main.py
@@ -12,19 +12,19 @@ while True:
     if user_input == '1':
         while True:
             try:
-                from ShellArmy.windows import win_defend
+                from windows import win_defend
             except SystemExit:
                 break
     elif user_input == '2':
         while True:
             try:
-                from ShellArmy.windows import win_analysis
+                from windows import win_analysis
             except SystemExit:
                 break
     elif user_input == '3':
         while True:
             try:
-                from ShellArmy.windows import win_log_auditing
+                from windows import win_log_auditing
             except SystemExit:
                 break
     elif user_input in ['4', 'back', 'Back']:
